af3e440ad91f6d0f94902a838d66fa775537d88b
Mostafa Zain <69126933+Mostafa-G-Zain@users.noreply.github.com>
2025-11-16 23:01:01 +0200

    Create P.cs

---
+++ Intermediate/P.cs
@@ -0,0 +1,36 @@
+/**
+ *  Author: ZainJr.
+ *  Created: 2025-11-16
+ **/
+global using static System.Console;
+using System.Numerics;
+
+namespace MyDraft
+{
+    class Program
+    {
+        public static Int32 Main()
+        {
+#if DEBUG
+            SetIn(new StreamReader("#input.txt"));
+            SetOut(new StreamWriter("#output.txt") { AutoFlush = true });
+#endif
+
+            /*CF*/
+            int TC = 1;
+            //TC = int.Parse(ReadLine());
+            while (TC-- > 0)
+            {
+                var lst = ReadLine().Split().Select(long.Parse).ToList();
+                long n = lst[0], k = lst[1];
+                WriteLine(((n & (1L << (int)k)) != 1) ? "YES" : "NO");  //isolate the bit
+                //WriteLine(((n >> (int)k) & 1) == 1 ? "YES" : "NO");     Mask
+            }
+            return 0;
+        }
+    }
+}
+/*
+
+
+*/
